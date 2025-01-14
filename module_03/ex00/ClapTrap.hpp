8c444f0017c04e18533b0a80c72b957c83289201
CABARBAYE Adrien <cabarbaye.adrien@gmail.com>
2025-01-14 04:42:38 +0100

    module 3 en cours

---
+++ module_03/ex00/ClapTrap.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acabarba <acabarba@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/07 15:45:09 by acabarba          #+#    #+#             */
+/*   Updated: 2025/01/07 15:47:55 by acabarba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap
+{
+private:
+	
+public:
+	ClapTrap();
+	~ClapTrap();
+};
+
+#endif
